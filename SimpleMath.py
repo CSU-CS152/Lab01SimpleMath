e08fdb44a117dac179eb4c6f6c81a715b9688a9f
Emilie <98420041+themilkprince@users.noreply.github.com>
2022-06-01 12:57:52 -0600

    Create SimpleMath.py

---
+++ SimpleMath.py
@@ -0,0 +1,18 @@
+def multiply(num1, num2):
+    return num1 * num2
+    
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def main():
+    someNum = int(input("Give me a number: "))
+    print(someNum)
+    anotherNum = int(input("Give me another number: "))
+    print(anotherNum)
+    print(subtract(add(multiply(someNum, anotherNum), someNum), anotherNum))
+
+if __name__ == "__main__":
+    main()
